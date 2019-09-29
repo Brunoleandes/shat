cd8e06bd1960b57e8c51c715fb1ae171c0ebb3b3
marianeco <pradomaria167@gmail.com>
2019-09-29 18:35:43 -0300

    criando servidor http

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
+require('dotenv').config();
+
 const WebSocket = require('ws');
 
 const express = require('express');
 
 const http = require('http');
+
+const app = express();
+
+app.get('/',function (req,res) {
+    res.status(200);
+    res.send('Hello World')
+})
+
+//Cria o servidor HTTP
+const httpServer = new http.Server(app)
+
+httpServer.listen(process.env.PORT,function (params) {
+    console.log('Servidor rondando em: http://localhost:'+process.env.PORT);
+})
