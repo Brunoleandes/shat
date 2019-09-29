939f6a325f684a27662b50216968a18a57b3101c
ayelsew <wesley.waaraujo@gmail.com>
2019-09-29 20:07:52 -0300

    front

--- chat.js
+++ chat.js
@@ -1,26 +1,35 @@
+//Pega as variáveis do .env para process.env
+require('dotenv').config();
+
 const WebSocket = require('ws');
 const username = process.env.USERNAME;
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 })
-readline.question(username + ' Digite o endereço do server', function (input) {
-    const wsc = new WebSocket('ws://' + input);
 
-    wsc.on('open', () => console.log('Conectado ao servidor'));
+const wsc = new WebSocket('ws://' + process.env.SRVIP);
+
+function clearShell(){
+    process.stdout.write('\u001B[2J\u001B[0;0f')
+}
+
+function CLI(params) {
+    readline.question(username + '>', function (input) {
+        wsc.send(input);
+        CLI();
+    });
+}
+
+wsc.on('open', function (params) {
+    clearShell();
+    readline.write('\nVocê foi conectado\n');   
+    CLI(); 
     
-    wsc.on('message', function (msg) {
-        console.log('Mensagem recebida: ' + msg)
-    })
+})
 
-
-
-
-    function CLI(params) {
-        readline.question(username + '>', function (input) {
-            wsc.send(input);
-            CLI();
-        });
-    }
+wsc.on('message', function (msg) {
+    readline.close();
+    console.log('\n<: ' + msg);
     CLI();
-});
+})
